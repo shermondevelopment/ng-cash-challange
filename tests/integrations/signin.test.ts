c3e41277fa5bf5576f117a402e35267b3bffc420
shermondevelopment <victorshermon@yahoo.com>
2022-11-16 21:01:59 -0300

    test: should receive error how status code 422 if username is empty

---
+++ tests/integrations/signin.test.ts
@@ -0,0 +1,29 @@
+/** supertest */
+import supertest from 'supertest'
+
+/** connection with prisma */
+import prisma from '../../app/config/prisma'
+
+/** server */
+import app from '../../app/config/server'
+
+describe('Signin', () => {
+  beforeEach(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE transactions`
+  })
+
+  afterAll(async () => {
+    await prisma.$disconnect()
+  })
+
+  it('should call router /signin how username empty and receive error with status ', async () => {
+    const response = await supertest(app).post('/signin').send({
+      username: '',
+      password: 'Senha@0101'
+    })
+
+    expect(response.status).toBe(422)
+    expect(response.body).toEqual({ error: 'please enter a username' })
+  })
+})
