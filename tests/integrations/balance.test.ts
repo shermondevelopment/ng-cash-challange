574a75dcaca2d430ae5427ac528b7b91ce8c7c5f
shermondevelopment <victorshermon@yahoo.com>
2022-11-19 18:38:36 -0300

    test: should call router /balance how badly formatted token and receive error 401

---
+++ tests/integrations/balance.test.ts
@@ -0,0 +1,22 @@
+/** supertest */
+import supertest from 'supertest'
+
+/** faker */
+import { faker } from '@faker-js/faker'
+
+/** server */
+import app from '../../app/config/server'
+
+/** factories */
+import token from '../factories/token-factory'
+
+describe('Balance', () => {
+  it('should call router /balance how badly formatted token and receive error 401', async () => {
+    const response = await supertest(app)
+      .get('/balance')
+      .send()
+      .set('Authorization', token(faker.datatype.uuid()))
+    expect(response.status).toBe(401)
+    expect(response.body).toEqual({ error: 'badly formatted token' })
+  })
+})
