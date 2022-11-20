5f3ddb24c5b0aa71ac27fdcb145a62410acac056
shermondevelopment <victorshermon@yahoo.com>
2022-11-20 11:59:42 -0300

    test: should call router /cash-out with username empty and receive erro 422

--- tests/factories/user-factory.ts
+++ tests/factories/user-factory.ts
@@ -5,7 +5,7 @@ import prisma from '../../app/config/prisma'
 import bcrypt from 'bcrypt'
 
 export const createUser = async () => {
-  await prisma.users.create({
+  return await prisma.users.create({
     data: {
       username: 'mockuser',
       password: await bcrypt.hash('Senha@0101', 12),
---
+++ tests/integrations/cashOut.test.ts
@@ -0,0 +1,38 @@
+/** supertes */
+import supertest from 'supertest'
+
+/** connection with prisma */
+import prisma from '../../app/config/prisma'
+
+/** app */
+import app from '../../app/config/server'
+
+/** factorys */
+import token from '../factories/token-factory'
+import { createUser } from '../factories/user-factory'
+
+let idUser = ''
+
+describe('CashOut', () => {
+  beforeAll(async () => {
+    const user = await createUser()
+    idUser = user.id
+  })
+  afterAll(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE transactions`
+    await prisma.$executeRaw`TRUNCATE TABLE accounts RESTART IDENTITY CASCADE`
+    await prisma.$disconnect()
+  })
+  it('should call router /cash-out with username empty and receive erro 422', async () => {
+    const response = await supertest(app)
+      .post('/cash-out')
+      .send({
+        username: '',
+        value: 2
+      })
+      .set('Authorization', `Bearer ${token(idUser)}`)
+    expect(response.status).toBe(422)
+    expect(response.body).toEqual({ error: 'Please enter your username' })
+  })
+})
